2c43dd3395dad1e302c0c272e835d32e19368a99
David M <dminerur@outlook.com>
2023-03-23 17:20:47 -0400

    calculator element added, before js and style adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,30 @@ function App() {
   return (
     <div className="App">
       <h1>JS + React: Calculator</h1>
+      <main class="calculator">
+        <output class="screen"></output>
+        <div class="buttons container">
+            <section class="numbers">
+                <button class="num 1">1</button>
+                <button class="num 2">2</button>
+                <button class="num 3">3</button>
+                <button class="num 4">4</button>
+                <button class="num 5">5</button>
+                <button class="num 6">6</button>
+                <button class="num 7">7</button>
+                <button class="num 8">8</button>
+                <button class="num 9">9</button>
+            </section>
+            <section class="operations">
+                <button class="op add">+</button>
+                <button class="op sub">-</button>
+                <button class="op mult">*</button>
+                <button class="op div">/</button>
+                <button class="op eq">=</button>
+            </section>
+
+        </div>
+    </main>
     </div>
   )
 }
