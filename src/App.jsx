e31e89656824dceca1f9de054fb8ea6dee515e93
David M <dminerur@outlook.com>
2023-03-27 17:19:49 -0400

    num btns and clear now functional

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,50 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0);
+
+  function handleClear() { 
+      setInput(0)
+  }
+
+  function handleClick(number) {
+    // Multiply the current value by 10, to shift the digits to the left, which creates placeholder for the new digit. Then, by adding the clicked number, we insert the new digit into the ones place.
+    setInput(input * 10 + number);
+  }
+
+
+  // const CalculatorOperations = {
+  //   '/': (prevValue, nextValue) => prevValue / nextValue,
+  //   '*': (prevValue, nextValue) => prevValue * nextValue,
+  //   '+': (prevValue, nextValue) => prevValue + nextValue,
+  //   '-': (prevValue, nextValue) => prevValue - nextValue,
+  //   '=': (prevValue, nextValue) => nextValue
+  // }
+
 
   return (
     <div className="App">
       <h1>JS + React: Calculator</h1>
-      <main class="calculator">
-        <output class="screen" id="display">12</output>
-        <div class="buttons container">
-                <button class="num 0" id="zero">0</button>
-                <button class="num 1" id="one">1</button>
-                <button class="num 2" id="two">2</button>
-                <button class="num 3" id="three">3</button>
-                <button class="num 4" id="four">4</button>
-                <button class="num 5" id="five">5</button>
-                <button class="num 6" id="six">6</button>
-                <button class="num 7" id="seven">7</button>
-                <button class="num 8" id="eight">8</button>
-                <button class="num 9" id="nine">9</button>
-                <button class="op add" id="add">+</button>
-                <button class="op sub" id="subtract">-</button>
-                <button class="op mult" id="multiply">x</button>
-                <button class="op div" id="divide">/</button>
-                <button id="equals" class="op eq">=</button>
-                <button id="decimal" class="deci">.</button>
-                <button id="clear" class="eraser" onClick="eraser()">CLEAR</button>
+      <main className="calculator">
+        <output className="screen" id="display">{input}</output>
+        <div className="buttons container">
+                <button className="num 0" id="zero" onClick={() => handleClick(0)}>0</button>
+                <button className="num 1" id="one" onClick={() => handleClick(1)}>1</button>
+                <button className="num 2" id="two" onClick={() => handleClick(2)}>2</button>
+                <button className="num 3" id="three" onClick={() => handleClick(3)}>3</button>
+                <button className="num 4" id="four" onClick={() => handleClick(4)}>4</button>
+                <button className="num 5" id="five" onClick={() => handleClick(5)}>5</button>
+                <button className="num 6" id="six" onClick={() => handleClick(6)}>6</button>
+                <button className="num 7" id="seven" onClick={() => handleClick(7)}>7</button>
+                <button className="num 8" id="eight" onClick={() => handleClick(8)}>8</button>
+                <button className="num 9" id="nine" onClick={() => handleClick(9)}>9</button>
+                <button className="op add" id="add">+</button>
+                <button className="op sub" id="subtract">-</button>
+                <button className="op mult" id="multiply">x</button>
+                <button className="op div" id="divide">/</button>
+                <button id="equals" className="op eq">=</button>
+                <button id="decimal" className="deci">.</button>
+                <button id="clear" className="eraser" onClick={handleClear}>CLEAR</button>
         </div>
     </main>
     </div>
