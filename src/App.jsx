733f0abe835a7502109f8bab61da5cbfbb0d0eb4
David M <dminerur@outlook.com>
2023-03-27 19:04:21 -0400

    added style, functionally sound. passing 12 of 16 tests

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,92 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [input, setInput] = useState(0);
+  const [input, setInput] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [prevInput, setPrevInput] = useState(null);
+  const [hasDecimal, setHasDecimal] = useState(false);
 
-  function handleClear() { 
-      setInput(0)
+
+
+  function handleClear() {
+    setInput("0");
+    setOperator(null);
+    setPrevInput(null);
+    setHasDecimal(false);
   }
 
   function handleClick(number) {
-    // Multiply the current value by 10, to shift the digits to the left, which creates placeholder for the new digit. Then, by adding the clicked number, we insert the new digit into the ones place.
-    setInput(input * 10 + number);
+    if (input === "0" && number !== ".") {
+      setInput(number.toString());
+    } else if (input === "-0" && number !== ".") {
+      setInput("-" + number.toString());
+    } else if (input === "" && number === ".") {
+      setInput("0.");
+    } else if (input.includes(".") && number === ".") {
+      return;
+    } else {
+      setInput(input.toString() + number.toString());
+    }
   }
 
+  function handleOperatorClick(operator) {
+    setHasDecimal(false);
+    if (prevInput === null) {
+      setPrevInput(input);
+    } else if (operator && prevInput && input) {
+      // perform the previous operation and update the display
+      const currentValue = parseFloat(input);
+      let result;
+      if (operator === "+") {
+        result = parseFloat(prevInput) + currentValue;
+      } else if (operator === "-") {
+        result = parseFloat(prevInput) - currentValue;
+      } else if (operator === "*") {
+        result = parseFloat(prevInput) * currentValue;
+      } else if (operator === "/") {
+        result = parseFloat(prevInput) / currentValue;
+      }
+      setInput(result.toString());
+      setPrevInput(result.toString());
+    }
+    setOperator(operator);
+    setInput("0");
+  }
 
-  // const CalculatorOperations = {
-  //   '/': (prevValue, nextValue) => prevValue / nextValue,
-  //   '*': (prevValue, nextValue) => prevValue * nextValue,
-  //   '+': (prevValue, nextValue) => prevValue + nextValue,
-  //   '-': (prevValue, nextValue) => prevValue - nextValue,
-  //   '=': (prevValue, nextValue) => nextValue
-  // }
+  function handleEqualsClick() {
+    if (prevInput === null) {
+      return;
+    }
+    const currentValue = parseFloat(input);
+    let result;
+    if (prevInput === null) {
+      return;
+    } else if (operator === "+") {
+      result = parseFloat(prevInput) + currentValue;
+    } else if (operator === "-") {
+      result = parseFloat(prevInput) - currentValue;
+    } else if (operator === "*") {
+      result = parseFloat(prevInput) * currentValue;
+    } else if (operator === "/") {
+      result = parseFloat(prevInput) / currentValue;
+    }
+    setPrevInput(null);
+    setOperator(null);
+    setInput(result.toString());
+    setHasDecimal(result.toString().includes('.'));
+  }
+
+  function handleDecimalClick() {
+    if (!hasDecimal) {
+      setHasDecimal(true);
+      setInput(input + ".");
+    }
+  }
 
 
   return (
     <div className="App">
-      <h1>JS + React: Calculator</h1>
+      <h1>Reactulator JS</h1>
       <main className="calculator">
         <output className="screen" id="display">{input}</output>
         <div className="buttons container">
@@ -41,12 +103,12 @@ function App() {
                 <button className="num 7" id="seven" onClick={() => handleClick(7)}>7</button>
                 <button className="num 8" id="eight" onClick={() => handleClick(8)}>8</button>
                 <button className="num 9" id="nine" onClick={() => handleClick(9)}>9</button>
-                <button className="op add" id="add">+</button>
-                <button className="op sub" id="subtract">-</button>
-                <button className="op mult" id="multiply">x</button>
-                <button className="op div" id="divide">/</button>
-                <button id="equals" className="op eq">=</button>
-                <button id="decimal" className="deci">.</button>
+                <button className="op add" id="add" onClick={() => handleOperatorClick('+')}>+</button>
+                <button className="op sub" id="subtract" onClick={() => handleOperatorClick('-')}>-</button>
+                <button className="op mult" id="multiply" onClick={() => handleOperatorClick('*')}>*</button>
+                <button className="op div" id="divide" onClick={() => handleOperatorClick('/')}>/</button>
+                <button id="equals" className="op eq" onClick={() => handleEqualsClick()}>=</button>
+                <button id="decimal" className="deci" onClick={() => handleClick('.')}>.</button>
                 <button id="clear" className="eraser" onClick={handleClear}>CLEAR</button>
         </div>
     </main>
