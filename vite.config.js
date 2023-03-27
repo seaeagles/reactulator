f6c7bdcfff67d2e17f86cd76094dac2f9c06c8f2
David M <dminerur@outlook.com>
2023-03-27 19:11:10 -0400

    added docs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/reactulator",
+    build: {
+      outDir: 'docs'
+    }
 })
